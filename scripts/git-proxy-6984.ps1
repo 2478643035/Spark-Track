74d5d6c970171173bf79193cf52b0b295c27727e
2478643035 <2478643035@qq.com>
2026-04-15 23:09:24 +0800

    Stop tracking local git proxy scripts

--- scripts/git-proxy-6984.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-git config --global http.proxy http://127.0.0.1:6984
-git config --global https.proxy http://127.0.0.1:6984
-
-Write-Host "Git proxy switched to 127.0.0.1:6984"
